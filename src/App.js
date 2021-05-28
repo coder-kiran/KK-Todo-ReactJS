cc4435f7b4023bea7630ad84a2a162aaf26ca6e7
Kiran K K <kirankalak4@gmail.com>
2021-05-28 10:52:37 -0700

    add button is added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Formforinput from './components/Formforinput';
 
 function App() {
   // declaring states
-  const [inputText,setInputText]=useState('')
-  return (
-    <div className="App">     
-    <header>
-     <h1> KK's todo list</h1>  
-    </header>
+  const [inputText, setInputText] = useState('')
+  const [todoslist, setTodoslist] = useState([])
 
-    <Formforinput inputText={inputText} setInputText={setInputText}/>
-         </div>
+  return (
+    <div className="App">
+      <header>
+        <h1> KK's todo list</h1>
+      </header>
+
+      <Formforinput inputText={inputText} setInputText={setInputText} todoslist={todoslist} setTodoslist={setTodoslist} />
+    </div>
   );
 }
 
--- src/components/Formforinput.js
+++ src/components/Formforinput.js
@@ -1,16 +1,28 @@
 import React from 'react'
 
-function Formforinput({setInputText,inputText}) {
+function Formforinput({setInputText,inputText,setTodoslist,todoslist}) {
+
+    // functions
     const inputHandler = (event) =>{
         console.log("value typed in input textbox",event.target.value)
         setInputText(event.target.value)
         
     }
+    
+    const submitTodoHandler = (event) =>{
+        event.preventDefault();
+        setTodoslist([...todoslist,{id:Date.now(),completed:false,text:inputText}])
+        console.log("value inserted to the todos list as array")     
+}
+
     return (
-        <div>
-           <input onChange={inputHandler} type="text" /> 
-           {console.log("value of state name inputText: ",inputText)}
-        </div>
+        <form>
+           <input onChange={inputHandler} type="text" value={inputText}  /> 
+           {console.log("value of state named inputText: ",inputText)}
+
+           <button onClick={submitTodoHandler}>Add</button>
+          
+        </form>
     )
 }
 
