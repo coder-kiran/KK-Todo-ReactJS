b9eabd445db12e1ed6699f053c8a2c3d69fba4e9
Kiran K K <kirankalak4@gmail.com>
2021-05-28 12:25:02 -0700

    done functionality is perfectly working.When pressed done button a strike is created through the text and opacity decreses

--- src/components/Todosboxes.js
+++ src/components/Todosboxes.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import './Todosboxes.css'
 
-function Todosboxes({ todoslist, setTodoslist }) {
+function Todosboxes({ todoslist, setTodoslist }) { 
+   
     return (
         <div>
             <ul>
                 {
                     todoslist.map((item) => {
+                         // functions
+                          const todoDone = () =>{
+                             console.log('item',item); // item {id: 1622226419487, completed: false, text: "home work"}
+                             
+                             // note that we are globally setting it using setTodolist
+                             setTodoslist(   
+                                todoslist.map((item2)=>{
+                                    console.log('item2',item2);  // item2 {id: 1622226419487, completed: false, text: "home work"}
+                                    if(item2.id === item.id){
+                                        return{
+                                         ...item2, completed:!item2.completed
+                                         
+                                        }
+                                          
+                                    }  
+                                  return item2 // why?
+                                                            
+                                })
+                              )  
+                         
+                    }
                         return (
-                            <li>{item.text}</li>
+                            
+                            <div key={item.id} className="list-body">
+                            {console.log("item completed status : ",item.completed)}                    
+                            <div className="">
+                            <li className={`${item.completed ? "completed" : ""}`}>{item.text}</li>
+                            </div>                            
+                            <button onClick={todoDone} >Done</button>
+                         
+                            {/* <button>Delete</button> */}
+                            </div>                         
+                           
                         )
                     })
                 }
